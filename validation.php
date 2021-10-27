434c2206a9250460ce2288b3b0bf03dee785f13e
jaikrishna <84390083+jaikrishnavr@users.noreply.github.com>
2021-10-27 23:32:02 +0530

    Update validation.php

--- validation.php
+++ validation.php
@@ -3,7 +3,7 @@
 session_start();
 
 
-$con=mysqli_connect('remotemysql.com','V9jXftGKr3','ygC3UQB4Gw','V9jXftGKr3');
+$con=mysqli_connect('localhost','root');
 
 if ($con) 
   {
@@ -14,7 +14,7 @@ else
 		echo "no connection";
 	}
 
- mysqli_select_db($con,'V9jXftGKr3');
+ mysqli_select_db($con,'uniquedeveloper');
  $name=$_POST['name'];
  $pass=$_POST['password'];
  $email=$_POST['email'];
